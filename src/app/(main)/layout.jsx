4af544d0446bf88bcf111d49a96ec2e202050a7f
Jahid150 <jahidpcbd@gmail.com>
2026-02-16 11:50:01 +0600

    2 layout created and footer created

---
+++ src/app/(main)/layout.jsx
@@ -0,0 +1,15 @@
+import Footer from '@/components/Footer/Footer';
+import Navbar from '@/components/Header/Navbar';
+import React from 'react';
+
+const layout = ({children}) => {
+  return (
+    <div>
+      <Navbar></Navbar>
+      {children}
+      <Footer></Footer>
+    </div>
+  );
+};
+
+export default layout;
