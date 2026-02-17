06e39a2f625451edb7aaef1c187a00a4da7f76f8
iamshakil01 <iamshakill02@gmail.com>
2026-02-17 15:40:58 +0600

    added navbar

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,9 +1,13 @@
 import Banner from '@/components/Banner/page';
+import Navbar from '@/components/Navbar/page';
 import React from 'react';
 
 const mainPage = () => {
   return (
     <div className="space-y-20" >
+      <section>
+        <Navbar></Navbar>
+      </section>
       <section>
         <Banner></Banner>
       </section>
