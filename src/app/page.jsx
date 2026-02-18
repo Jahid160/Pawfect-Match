483da5e7d9c81bed06c832afcaa9f675841f5901
Hasib Shrabon <hasibahmedshrabon@gmail.com>
2026-02-18 14:35:13 +0600

    [ADDED]:Pet Categories section

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,16 +1,15 @@
-
-import Banner from '@/components/home/Banner';
-import React from 'react';
+import Banner from "@/components/home/Banner";
+import PetCategories from "@/Components/home/PetCategories";
+import React from "react";
 
 const mainPage = () => {
   return (
-    <div className="space-y-5" >
+    <div className="space-y-5">
       <section>
         <Banner></Banner>
+        <PetCategories></PetCategories>
       </section>
     </div>
-
-
   );
 };
 
