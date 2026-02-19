4303db6643836361db11e433441e57c26d60d2ed
alminsfd <alaminhossaintanvir42@gmail.com>
2026-02-19 08:48:10 +0600

    Issue solve

--- src/app/register/page.jsx
+++ src/app/register/page.jsx
@@ -1,11 +1,10 @@
-
 import { RegisterForm } from "@/Components/auth/RegisterForm";
 import React from "react";
 
 const RegisterPage = () => {
   return (
     <div>
-      <RegisterForm/>
+      <RegisterForm />
     </div>
   );
 };
