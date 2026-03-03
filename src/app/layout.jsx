38080518b03912c67321d1da9cf65660d2b9f4e0
Hasib Shrabon <hasibahmedshrabon@gmail.com>
2026-03-03 16:51:10 +0600

    Added: Live ChatBot Ai Done

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -3,7 +3,7 @@ import "./globals.css";
 import NextAuthProvider from "@/provider/NextAuthProvider";
 import Navbar from "@/components/Header/Navbar";
 import Footer from "@/components/Footer/Footer";
-
+import Script from "next/script";
 
 const poppins = Poppins({
   subsets: ["latin"],
@@ -24,7 +24,14 @@ export default function RootLayout({ children }) {
             <Navbar></Navbar>
           </header>
 
-          <main className="mx-auto py-2 md:w-11/12 min-h-[calc(100vh-302px)]">{children}</main>
+          <main className="mx-auto py-2 md:w-11/12 min-h-[calc(100vh-302px)]">
+            {children}
+          </main>
+          <script
+            src="//code.tidio.co/bnnskbd4agggqkgqz1jtpiuiiw8yt0s7.js"
+            strategy="afterInteractive"
+            async
+          ></script>
 
           <footer>
             <Footer></Footer>
