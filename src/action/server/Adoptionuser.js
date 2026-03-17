000e5b7477eb7a1728d4f076dcadf92afdf34764
Jahid150 <jahidpcbd@gmail.com>
2026-03-17 22:48:20 +0600

    pet management id done and almost 70% done

--- src/action/server/Adoptionuser.js
+++ src/action/server/Adoptionuser.js
@@ -1,15 +1,67 @@
-'use server'
+"use server";
+
+import { userVerifyAuth } from "@/lib/userVerifyAuth";
+import { ObjectId } from "mongodb";
 
 const { dbConnect, collections } = require("@/lib/db");
 
-const petCollectionPromise = dbConnect(collections.ADOPTIONS);
+const adoptionCollectionPromise = dbConnect(collections.ADOPTIONS);
+
+const petCollectionPromise = dbConnect(collections.PETS);
+
+const generateId = () => {
+  const chars =
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+  let id = "";
+
+  for (let i = 0; i < 10; i++) {
+    id += chars.charAt(Math.floor(Math.random() * chars.length));
+  }
+
+  return id;
+};
 
 export const createAdoptionUser = async (data) => {
-     try {
-          const petCollection = await petCollectionPromise;
-          const result = await petCollection.insertOne(data);
-          return { success: true, id: result.insertedId.toString() };
-     } catch (error) {
-          return { success: false, error: error.message };
-     }
-}
+  try {
+    console.log(data);
+    let user;
+    try {
+      user = await userVerifyAuth();
+    } catch (error) {
+      return { success: false, message: error.message };
+    }
+
+    const userEmail = user.email;
+    console.log("Action performed by:", userEmail);
+
+    const adoptionCollection = await adoptionCollectionPromise;
+    const petCollection = await petCollectionPromise;
+
+    // ✅ generate 10 character id
+    const adoptionId = generateId();
+
+    await petCollection.updateOne(
+      { _id: new ObjectId(data.petId) },
+      {
+        $set: {
+          status: "pending",
+          adoptedUserEmail: userEmail,
+          adoptionCode: adoptionId,
+        },
+      },
+    );
+
+    const result = await adoptionCollection.insertOne({
+      ...data,
+      adoptionCode: adoptionId,
+    });
+
+    return {
+      success: true,
+      id: result.insertedId.toString(),
+      adoptionCode: adoptionId,
+    };
+  } catch (error) {
+    return { success: false, error: error.message };
+  }
+};
